834dc143b18499f591013079137fe684eb909476
Alex Shulgin <ash@commandprompt.com>
2012-05-23 12:01:36 -0700

    Drop empty self.extended method in UserPatch.

--- lib/redmine_lock_email_notification_options/user_patch.rb
+++ lib/redmine_lock_email_notification_options/user_patch.rb
@@ -12,9 +12,6 @@ module RedmineLockEmailNotificationOptions
     end
 
     module ClassMethods
-      def self.extended(base)
-      end
-
       def valid_notification_options_with_lock(user=nil)
         valid_notification_options_without_lock(user).tap do |options|
           options.reject! {|o| o.first != Setting.default_notification_option} unless User.current.admin?
