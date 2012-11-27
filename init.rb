36c6e18166afd4ed28b0c9eb237a125b850d70f8
Alex Shulgin <ash@commandprompt.com>
2012-11-27 13:00:12 -0800

    Redmine-2.x compatibility.

--- init.rb
+++ init.rb
@@ -1,17 +1,20 @@
 require 'redmine'
-require 'dispatcher'
-
-Dispatcher.to_prepare do
-  require_dependency 'user'
-
-  User.send(:include, RedmineLockEmailNotificationOptions::UserPatch)
-end
 
 Redmine::Plugin.register :redmine_lock_email_notification_options do
   name 'Redmine Lock Email Notification Options plugin'
   author 'Alex Shulgin <ash@commandprompt.com>'
   description 'Lock user account email notification option at the default setting'
-  version '0.0.1'
+  version '0.2.0'
 #  url 'http://example.com/path/to/plugin'
 #  author_url 'http://example.com/about'
+end
+
+prepare_block = Proc.new do
+  User.send(:include, RedmineLockEmailNotificationOptions::UserPatch)
+end
+
+if Rails.env.development?
+  ActionDispatch::Reloader.to_prepare { prepare_block.call }
+else
+  prepare_block.call
 end
